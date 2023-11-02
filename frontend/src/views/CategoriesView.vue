2ef106e8631d072e41c61298b8924b0ac89ff8c8
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-11-02 07:17:37 +0530

    Unused Added categories page

---
+++ frontend/src/views/CategoriesView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="category">
+    <img alt="Vue logo" src="../assets/Circular_game_of_life.png">
+    <CategoriesTable/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CategoriesTable from '@/components/CategoriesTable.vue';
+
+export default {
+  name: 'CategoriesUserView',
+  components: {
+    CategoriesTable
+  },
+}
+</script>
