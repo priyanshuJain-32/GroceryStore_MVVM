89f77c3c344b5983eaa2bacfed45f1388b86524f
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-11-02 07:19:57 +0530

    Added new links

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,8 +6,13 @@ const routes = [
   {path: '/about', name: 'about', component: () => import('../views/AboutView.vue')},
   {path: '/signup', name: 'signup', component: () => import('../views/SignUpView.vue')},
   {path: '/login', name: 'login', component: () => import('../views/LoginView.vue')},
+  {path: '/logout', name: 'logout', component: () => import('../views/LogoutView.vue')},
   {path: '/admin-login', name: 'admin-login', component: () => import('../views/LoginView.vue')},
-  {path: '/products-user-view', name: 'products-user-view', component: () => import('../views/ProductsUserView.vue')}
+  {path: '/categories-user-view', name: 'categories-user-view', component: () => import('../views/CategoriesView.vue')},
+  {path: '/products-user-view/', name: 'products-user-view', component: () => import('../views/ProductsView.vue')},
+  {path: '/buy-now-user-view/:product_id', name: 'buy-now-user-view', component: () => import('../views/BuyNowUserView.vue')},
+  {path: '/cart-user-view', name: 'cart-user-view', component: () => import('../views/BuyNowUserView.vue')},
+  {path: '/orders-user-view', name: 'orders-user-view', component: () => import('../views/BuyNowUserView.vue')}
 ]
 
 const router = createRouter({
