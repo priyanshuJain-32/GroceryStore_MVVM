cca294c4be426ca6fce1e1a6963bf617431c950b
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-11-02 07:21:30 +0530

    Made some upgrades
    Further developed the buy now page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,18 +6,24 @@
     <router-link v-if="token.jwt == ''" to="/login"> | User Login | </router-link>
     <router-link v-if="token.jwt == ''" to="/signup"> | Sign Up | </router-link>
     <router-link v-if="token.jwt == ''" to="/admin-login">| Admin Login | </router-link>
-    <router-link v-if="token.jwt !== ''" to="/products-user-view"> | Products | </router-link>
+    <router-link v-if="token.jwt !== ''" to="/categories-user-view"> | All Categories | </router-link>
+    <router-link v-if="token.jwt !== ''" to="/cart-user-view"> | Cart | </router-link>
+    <router-link v-if="token.jwt !== ''" to="/orders-user-view"> | Orders | </router-link>
+    <router-link @click="logout" v-if="token.jwt !== ''" to="/logout">| Logout |</router-link>
     |
   </nav>
   <router-view/>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 export default {
   name: 'App',
   computed: {
     ...mapGetters('auth',['token'])
+  },
+  methods: {
+    ...mapActions('auth',['logout'])
   }
 }
 </script>
