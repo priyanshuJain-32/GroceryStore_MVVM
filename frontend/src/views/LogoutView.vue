9e59e22f9ea40abf67bf00fe9caec84163d8f780
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-11-02 07:18:11 +0530

    Added logout page

---
+++ frontend/src/views/LogoutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="logout">
+    <img alt="Vue logo" src="../assets/Circular_game_of_life.png">
+    <h2> Thank You for visiting</h2>
+    <h3> Go to Login Page. If you wish to start again.</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'LogoutView'
+}
+</script>
