bd4debb0ec33a369099ea269b889f1881b71a0ea
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-11-02 07:13:38 +0530

    Made some changes

--- frontend/src/components/ProductsTable.vue
+++ frontend/src/components/ProductsTable.vue
@@ -1,39 +1,38 @@
 <template>
     <div class="products-table">
 
-<!-- 
-        <table id = "product-table" class="table" style="width: 95%; text-align: center;">
+		<table id = "product-table" class="table" style="width: 95%; text-align: center;">
+			<th>Category Id</th>
+			<th>Category</th>
 			<th>Product</th>
 			<th>Product Desc</th>
 			<th>Price/ Unit</th>
-			<th>Available Quantity</th>
+			<th v-if="params.role == 'manager' || params.role == 'admin'">Available Quantity</th>
 			<th>Discount %</th>
 			<th>Offer Price</th>
 			<th>Product Expiry Date</th>
 			<th></th>
 			<th></th>
 			
-			{% for product in category.products %}
-			<tr>
-			<td>{{ product.product_name }}</td>
-			<td>{{ product.product_desc }}</td>
-			<td>{{ product.sell_price }}</td>
-			<td>{{ product.product_quantity }}</td>
-			<td>{{ product.discount }}</td>
-			<td>{{ (product.sell_price|int)*(100-product.discount)/100 }}</td>
-			<td>{{ product.expiry_date }}</td>
-			{% if product.product_quantity!=0 %}
-			<td><form action="/order_product/{{ product.product_id }}" method="GET">
-				<input type="submit" style="width: 120px;" name="order_product" value = "Buy Now"/></form></td>
-			<td><form action="/cart_product/{{ product.product_id }}" method="POST">
-				<input type="submit" style="width: 150px;" name="cart_product" value = "Add to Cart"/></form></td>
+			<tr v-for="product in products.products" :key="product.product_id">
+				<div v-if="product.quantity==0">Out of Stock</div>
+					<td>{{ product.product_category_id }}</td>
+					<td>{{ product.product_category }}</td>
+					<td>{{ product.product_name }}</td>
+					<td>{{ product.product_desc }}</td>
+					<td>{{ product.sell_price }}</td>
+					<td v-if="params.role == 'manager' || params.role=='admin'">{{ product.product_quantity }}</td>
+					<td>{{ product.discount }}</td>
+					<td>{{ (product.sell_price|int)*(100-product.discount)/100 }}</td>
+					<td>{{ product.expiry_date }}</td>
+					
+					<td><router-link :to="'/buy-now-user-view/' + product.product_id">Buy Now</router-link></td>
+					<td><form>
+						<input type="button" @click="this.addToCart" style="width: 150px;" name="cart_product" value = "Add to Cart"/></form></td>
 			</tr>
-			{% else %}
-			<td>Out of Stock</td>
-			{% endif %}
-			{% endfor %} -->
-    
-    
+			<td></td>
+			
+		</table>
     </div>
 </template>
 
@@ -46,14 +45,13 @@ import { mapGetters, mapActions } from 'vuex';
 export default {
   name: 'ProductsTable',
   computed: {
-    ...mapGetters('auth',['params'])
+    ...mapGetters('auth',['params']),
+	...mapGetters('product',['products']),
+	...mapGetters('category',['categories'])
   },
   methods: {
-    submitSignup() {
-      this.signup(this.params);
-      this.registerUser();
-    },
-    ...mapActions('product',['registerUser'])
+	...mapActions('product',['buyNow']),
+	...mapActions('cart',['addToCart'])
   }
 }
 </script>
