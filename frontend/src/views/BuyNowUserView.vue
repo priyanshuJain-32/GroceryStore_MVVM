8029e11bad7d9e45110bc85308b6070a9a546598
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-11-02 07:16:22 +0530

    Added buy now page

---
+++ frontend/src/views/BuyNowUserView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="products">
+    <img alt="Vue logo" src="../assets/Circular_game_of_life.png">
+    <BuyNow/>
+  </div>
+</template>
+
+<script>
+
+import BuyNow from '@/components/BuyNow.vue';
+
+export default {
+  name: 'BuyNowUserView',
+  components: {
+    BuyNow
+  },
+}
+</script>
