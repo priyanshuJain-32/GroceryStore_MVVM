8029f9184f6bae1a27af6dfbee89f8207b496ac0
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-11-17 06:12:13 +0530

    Add files via upload

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,16 +1,18 @@
 import { createStore } from 'vuex'
-import auth from './modules/auth.js'
 import meta from './modules/meta.js'
+import auth from './modules/auth.js'
 import product from './modules/product.js'
-import location from './modules/location.js'
 import category from './modules/category.js'
+import order from './modules/order.js'
+import cart from './modules/cart.js'
 
 const store = createStore({ modules: {
     meta,  
     auth,
     product,
-    location,
     category,
+    order,
+    cart,
     
   } 
 })
