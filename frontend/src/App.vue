588702e9e0aae45553dfd2c4c2f03b88f6645e64
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-12-02 18:33:56 +0530

    Add files via upload

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,18 @@
-
-
-Working on adding admin login logic
-
 <template>
   <nav>
     |
     <router-link v-if="token.jwt == ''" to="/"> | Home | </router-link>
     <router-link to="/about"> | About | </router-link>
     <router-link @click="setUserRole('user')" v-if="token.jwt == ''" to="/login"> | User Login | </router-link>
-    <router-link v-if="token.jwt == ''" to="/signup"> | Sign Up | </router-link>
-    <router-link @click="setUserRole('admin')" v-if="token.jwt == ''" to="/admin-login">| Admin Login | </router-link>
-    <!-- <router-link v-if="token.jwt !== ''" to="/categories-user-view"> | All Categories | </router-link> -->
-    <router-link v-if="token.jwt !== ''" to="/products-user-view/"> | Products | </router-link>
-    <router-link v-if="token.jwt !== ''" to="/cart-user-view"> | Cart | </router-link>
-    <router-link v-if="token.jwt !== ''" to="/orders-user-view"> | Orders | </router-link>
-    <router-link @click="logout" v-if="token.jwt !== ''" to="/logout">| Logout |</router-link>
+    <router-link @click="setUserRole('user')" v-if="token.jwt == ''" to="/signup"> | Sign Up | </router-link>
+    <router-link @click="setUserRole('admin')" v-if="token.jwt == ''" to="/admin-login">| Staff Login | </router-link>
+    <router-link v-if="token.jwt !== '' && (params.role=='manager' || params.role=='admin')" to="/categories-user-view"> | All Categories | </router-link>
+    <router-link v-if="token.jwt !== '' && params.role == 'user'" to="/products-user-view/"> | Products | </router-link>
+    <router-link v-if="(token.jwt !== '') && (params.role=='manager' || params.role=='admin')" to="/dashboard-staff-view"> | Dashboard | </router-link>
+    <router-link v-if="(token.jwt !== '') && (params.role=='manager' || params.role=='admin')" to="/products-staff-view"> | Products | </router-link>
+    <router-link v-if="token.jwt !== '' && params.role == 'user'" to="/cart-user-view"> | Cart | </router-link>
+    <router-link v-if="token.jwt !== '' && params.role == 'user'" to="/orders-user-view"> | Orders | </router-link>
+    <router-link @click="logout()" v-if="token.jwt !== ''" to="/logout">| Logout |</router-link>
     |
   </nav>
   <router-view/>
@@ -25,7 +23,7 @@ import { mapGetters, mapActions, mapMutations } from 'vuex';
 export default {
   name: 'App',
   computed: {
-    ...mapGetters('auth',['token'])
+    ...mapGetters('auth',['token', 'params'])
   },
   methods: {
     ...mapActions('auth',['logout']),
